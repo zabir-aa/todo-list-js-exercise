94c6a883c45097a8916128da027095a11916be57
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-08-15 04:07:29 -0400

    Completed toDoList with help from the walkthrough - OOP style

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
@@ -29,3 +30,32 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+
+*/
+
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+}
+
+// DRIVER CODE CHANGES BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
